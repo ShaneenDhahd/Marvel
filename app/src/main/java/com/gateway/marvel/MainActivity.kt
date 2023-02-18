f10edf816b01b581a53ac14ec28aac1f3ec53b8a
Shaneen Dhahd <hadidhahds@gmail.com>
2023-02-18 17:24:45 +0300

    adding libraries

--- app/src/main/java/com/gateway/marvel/MainActivity.kt
+++ app/src/main/java/com/gateway/marvel/MainActivity.kt
@@ -3,13 +3,9 @@ package com.gateway.marvel
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.gateway.marvel.ui.theme.MarvelTheme
 
 class MainActivity : ComponentActivity() {
